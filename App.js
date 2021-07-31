78bcc024047608ea5a85b898fa39decc2657c924
Babar ali <babarali@arcocia.tech>
2021-07-31 17:05:57 +0500

    Second commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  console.log('App Executed')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native!</Text>
+      <TouchableOpacity onPress={()=>console.log('image pressed')}>
+      <Image 
+      blurRadius={3}
+      fadeDuration={1024}
+      source={{
+        width: 200,
+        height:300,
+        uri:'https://source.unsplash.com/random'}} />
+
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
